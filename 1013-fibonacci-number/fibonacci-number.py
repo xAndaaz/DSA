c85653d9355cd516ec572feb190bc402d9aa9029
Andaaz <149862517+xAndaaz@users.noreply.github.com>
2025-09-06 00:52:34 +0530

    Time: 32 ms (91.06%) | Memory: 17.9 MB (23.32%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        x= n+2
+        f = [0]*x
+
+        f[0]= 0
+        f[1] = 1
+        for i in range (2,n+1):
+            f[i] = f[i-1]+ f[i-2]
+        return f[n]    
