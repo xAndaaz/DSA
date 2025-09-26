583b5f3c5b97c2df445d9e4f5305273084b06a28
Andaaz <149862517+xAndaaz@users.noreply.github.com>
2025-09-26 11:51:31 +0530

    Time: 9 ms (76.78%) | Memory: 18.1 MB (34.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hs = {}
-        ht= {}
-        for index,item in enumerate (s):
-            if item in hs:
-                hs[item]+=1
-            else:
-                hs[item] =1     
-        for index,item in enumerate (t):
-            if item in ht:
-                ht[item]+=1
-            else:
-                ht[item] =1        
-        if ht==hs:
-            return True
-        return False    
+        count= defaultdict(int)
+        for i in s:
+            count[i] += 1
+        for i in t:
+            count[i]-= 1
+        for value in count.values():
+            if value !=0 :
+                return False
+        return True
